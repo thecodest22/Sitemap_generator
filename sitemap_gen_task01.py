84d3faf249a0d8ae52326444d290a845f0863cb0
codest <thecodest@ya.ru>
2023-04-13 21:22:33 +0300

    Базовый функционал без наворотов

---
+++ sitemap_gen_task01.py
@@ -0,0 +1,33 @@
+from itertools import islice
+from pathlib import Path
+
+from lxml import etree
+
+URL_PRIORITY = '0.3'
+TARGET_NODE = '//offer/url'
+FILENAME_PREFIX = 'sitemap'
+
+path_to_xml_file = Path(input('Укажите путь к файлу выгрузки: ')).resolve()
+parsed_xml_tree = etree.parse(path_to_xml_file)
+
+iterator = parsed_xml_tree.iterfind(TARGET_NODE)
+
+sitemap_file_number = 1
+while True:
+    sitemap_root = etree.Element('urlset', xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+    sitemap_tree = etree.ElementTree(sitemap_root)
+
+    for i_node in islice(iterator, 50000):
+        sitemap_url = etree.SubElement(sitemap_root, 'url')
+        etree.SubElement(sitemap_url, 'loc').text = i_node.text
+        etree.SubElement(sitemap_url, 'priority').text = URL_PRIORITY
+
+    if not len(sitemap_tree.getroot()):
+        break
+
+    output_sitemap_file = Path() / f'{FILENAME_PREFIX}_{str(sitemap_file_number).zfill(2)}.xml'
+    with output_sitemap_file.open('wb') as xml_output:
+        sitemap_tree.write(xml_output, pretty_print=True, encoding='utf-8', xml_declaration=True)
+
+    sitemap_file_number += 1
+
